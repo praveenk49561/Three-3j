e610a712864e080673413728aa7ed23458c4955d
Praveen Kumar <66628434+praveenk49561@users.noreply.github.com>
2023-11-29 22:22:59 +0530

    first

---
+++ main.js
@@ -0,0 +1,14 @@
+
+import src from './src_stars';
+import { gsap } from 'gsap/gsap-core';
+import './index.css';
+
+src();
+
+// const text = document.getElementById("heading");
+
+//var tl = new TimelineLite();
+//tl.to(element, 1, {width:"50%"});
+
+// const tl = gsap.timeline({repeat:-1, yoyo:false, repeatDelay:0, onComplete:timelineDone, onCompleteParams:["test1", "test2"]});
+
